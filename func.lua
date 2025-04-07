c7eed6eaef95f80e12c09df4af3011f15991fc44
mammothkung <tongmo.jingithub@gmail.com>
2025-04-07 10:56:57 +0700

    Update func.lua

--- func.lua
+++ func.lua
@@ -1,11 +1,9 @@
 local function FindAllDescendantsByName(parent, nameToFind)
-    local matchingDescendants = {}
     for _, descendant in ipairs(parent:GetDescendants()) do
         if descendant.Name == nameToFind then
-            table.insert(matchingDescendants, descendant)
+            return descendant
         end
     end
-    return matchingDescendants
 end
 
 local PathModule = {}
